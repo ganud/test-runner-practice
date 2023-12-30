be8aeba09e8fca486fb13d0ef69fa36c04e891ce
Dundo <107737568+ganud@users.noreply.github.com>
2023-12-30 11:58:30 -0500

    Add calculator

---
+++ calculator.test.js
@@ -0,0 +1,16 @@
+import { Calculator } from './calculator';
+
+const calculator = new Calculator()
+
+test('Add 2 + 2', () => {
+  expect(calculator.add(2, 2)).toBe(4);
+});
+test('Minus 2 - 2', () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+});
+test('Mult 2 * 2', () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+});
+test('Div 2 / 2', () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+});
